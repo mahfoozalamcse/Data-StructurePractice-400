044f0a3e7fe0af06d876dab1b336483bd52d3cfb
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-12-21 15:47:26 +0000

    learn IntroArray

---
+++ IntroArray/Intro.java
@@ -0,0 +1,44 @@
+public class Intro {
+
+    // 1. Introduction to Arrays in Java
+
+    // Arrays are one of the most fundamental data structures in Java.
+    // They allow us to store multiple values of the same type in a single variable and access them efficiently using an index.
+
+
+//     Definition of Arrays ->
+//    An array is a collection of elements of the same data type, stored in contiguous memory locations.
+//    Each element in the array can be accessed using its index, starting from 0.
+
+
+// Memory Representation
+// In Java, arrays are implemented as objects. The memory is allocated in the heap, 
+// and a reference to the array is stored in the stack.
+
+// For example:
+
+// int[] arr = new int[5];
+
+// Here:
+
+// A reference to the array object is stored in the stack.
+// The array object itself is allocated in the heap.
+// Memory layout for arr:
+
+// Index:   0   1   2   3   4
+// Values: [0] [0] [0] [0] [0]  (default values for integers)
+
+// Static vs. Dynamic Arrays
+// Static Arrays:
+
+// The size of the array is fixed during initialization and cannot be changed later.
+// Example:
+    //  int[] staticArray = new int[5]; // Array size is fixed at 5
+   
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
