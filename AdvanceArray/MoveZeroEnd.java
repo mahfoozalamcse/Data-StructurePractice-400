32d55e1db816b9ad0bb3fe8e0f2182ef99985065
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-31 11:02:11 +0000

    move zero end try solve problem

---
+++ AdvanceArray/MoveZeroEnd.java
@@ -0,0 +1,10 @@
+public class MoveZeroEnd {
+    public static void moveZeroEnd1(int arr[]){
+       for (int i = 0; i < arr.length; i++) {
+           
+       }
+    }
+   public static void main(String[] args) {
+    
+   } 
+}
