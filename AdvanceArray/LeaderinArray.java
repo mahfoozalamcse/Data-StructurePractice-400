5d09bd708323dd7740d125729825acabee9f4c7e
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-02-13 07:56:09 +0000

    leader array efficient way

--- AdvanceArray/LeaderinArray.java
+++ AdvanceArray/LeaderinArray.java
@@ -32,12 +32,15 @@ public class LeaderinArray {
         }
     }
 
+    
     // print array
     public static void printArray(int arr[]){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
     }
+
+
 
     public static void main(String[] args) {
         int arr[] = {7, 10, 4, 3, 6, 5, 2};
