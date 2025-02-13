0ae6d15b22c3c616516d8087bfdcb85739944486
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-02-13 10:11:32 +0000

    part1 buy sell stock

---
+++ AdvanceArray/StockBSPart1.java
@@ -0,0 +1,11 @@
+public class StockBSPart1 {
+  public static int maxProfit(int price[], int start, int end){
+    return 0;
+    // solve next day 
+  }
+  public static void main(String[] args) {
+    int price[] = {1, 5, 3, 8, 12};
+    maxProfit(price, 0, 0);
+    
+  }  
+}
