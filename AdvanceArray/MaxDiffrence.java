384d5cddef280c478baeb674fc26dce1295bdf62
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-12 23:42:46 +0530

    Update MaxDiffrence.java

--- AdvanceArray/MaxDiffrence.java
+++ AdvanceArray/MaxDiffrence.java
@@ -4,7 +4,7 @@ public class MaxDiffrence {
   public static int maxDiffrence(int arr[]){
     
     int maxDiff = arr[1] - arr[0];
-    for(int i=0; i<arr.length; i++){
+    for(int i=0; i<arr.length; i++){ 
         
         for(int j=i+1; j<arr.length; j++){
              
