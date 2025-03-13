0ccc87d37ee813bbb2c2108dd2453acffc70e384
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-13 20:57:57 +0530

    Update FrequencyArray.java

--- AdvanceArray/FrequencyArray.java
+++ AdvanceArray/FrequencyArray.java
@@ -6,7 +6,7 @@ public class FrequencyArray {
     public static void printFrequency(int arr[]){ 
         int freq = 1, i = 1;
         while (i < arr.length) { 
-            
+             
          
         while (i < arr.length &&  arr[i] == arr[i-1]) {
             freq++;
