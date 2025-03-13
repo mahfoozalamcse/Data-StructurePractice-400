7d031acd692698682474376d25262f335053d793
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-13 20:58:10 +0530

    Update LeaderinArray.java

--- AdvanceArray/LeaderinArray.java
+++ AdvanceArray/LeaderinArray.java
@@ -4,7 +4,7 @@ public class LeaderinArray {
     // Leader in array Neive method O(n^2)
     public static void leader(int arr[]){
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)  {
             boolean flag = false;
              for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] <= arr[j]) {
