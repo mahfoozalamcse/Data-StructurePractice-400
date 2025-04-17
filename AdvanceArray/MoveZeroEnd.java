08c24ff4d4ad6292fa7380e987436d6275523b38
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-04-17 22:20:20 +0530

    Update MoveZeroEnd.java

--- AdvanceArray/MoveZeroEnd.java
+++ AdvanceArray/MoveZeroEnd.java
@@ -2,7 +2,7 @@ public class MoveZeroEnd {
     public static void moveZeroEnd1(int arr[]){
        for (int i = 0; i < arr.length; i++) { 
            
-       }
+       } 
     }
    public static void main(String[] args) {
     
