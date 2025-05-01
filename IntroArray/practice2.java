84849257ddc3033a7680ab8276921554f35c009b
mahfoozalamcse <amahfooz1007@gmail.com>
2025-05-01 15:45:47 +0530

    large,reverse, seclarge question practice

--- IntroArray/practice2.java
+++ IntroArray/practice2.java
@@ -27,14 +27,37 @@ public class practice2 {
         return max;
     }
 
-    // 
+    // Reverse array
+    public static void reverse(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // print array
+    public static void print(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
   public static void main(String[] args) {
     int arr[] = {12, 78, 90, 98, 77};
     // System.out.println();
     // System.out.println(largest(arr));
 
 
-    System.out.println();
-    System.out.println(secLarge(arr));
+    // System.out.println(); 
+    // System.out.print(secLarge(arr));
+    
+    reverse(arr);
+    print(arr);
+    
   }
 }
