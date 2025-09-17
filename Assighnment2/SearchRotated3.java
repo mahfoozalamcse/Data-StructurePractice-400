ca71d60a3ebf01330edcaf5be5d20f282b01295d
mahfoozalamcse <amahfooz1007@gmail.com>
2025-09-17 22:07:36 +0530

    practice leetcode 81 problem

---
+++ Assighnment2/SearchRotated3.java
@@ -0,0 +1,44 @@
+package Assighnment2;
+
+public class SearchRotated3 {
+    class Solution {
+        public boolean search(int[] nums, int target) {
+            int start = 0, end = nums.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (nums[mid] == target) {
+                    return true;
+                }
+                if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                    start++;
+                    end--;
+                }
+                // left
+                else if (nums[start] <= nums[mid]) {
+                    if (nums[start] <= target && target < nums[mid]) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+                // right
+                else {
+
+                    if (nums[mid] < target && target <= nums[end]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+     
+    }
+}
