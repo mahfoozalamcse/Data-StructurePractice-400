503f9fb05dd1143aadac1e2e1dff0ae6baefffac
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-09-21 19:39:27 +0530

    Update LeftRotateDPos.java

--- AdvanceArray/LeftRotateDPos.java
+++ AdvanceArray/LeftRotateDPos.java
@@ -9,6 +9,7 @@ public class LeftRotateDPos {
         }
         for(int i=d; i<n; i++){
             arr[i-d] = arr[i];
+            
         }
         for(int i=0; i<d; i++){
             arr[n-d+i] = temp[i];
