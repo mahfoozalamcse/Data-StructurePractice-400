860be75155ec63070e8c0f70583ddcf626570ced
mahfoozalamcse <amahfooz1007@gmail.com>
2025-09-24 15:17:10 +0530

    practice reverse string

---
+++ Assighnment4/ReverseString1.java
@@ -0,0 +1,26 @@
+package Assighnment4;
+// Leetcode 344. Reverse String
+
+public class ReverseString1 {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            start++;
+            end--;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseString1 reverseString1 = new ReverseString1();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString1.reverseString(s);
+        System.out.println(s); // Output: "olleh"
+    }
+}
