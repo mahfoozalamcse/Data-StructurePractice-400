a19c1ed0996d9e831cabfb250484a524a561aadf
mahfoozalamcse <amahfooz1007@gmail.com>
2025-09-24 15:26:29 +0530

    practice fibbonacci using recursion

---
+++ Assighnment4/Fibbonacci2.java
@@ -0,0 +1,18 @@
+package Assighnment4;
+// Leetcode 509. Fibonacci Number
+
+public class Fibbonacci2 {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Fibbonacci2 fibbonacci2 = new Fibbonacci2();
+        int n = 5;
+        int result = fibbonacci2.fib(n);
+        System.out.println(result); // Output: 5
+    }
+}
