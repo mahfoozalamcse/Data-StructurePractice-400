d8ec66d1d1a7e35c4a3961ceedaab9e6b4f324a2
mahfoozalamcse <amahfooz1007@gmail.com>
2025-10-21 07:11:41 +0530

    practice recursion to reverse 1

--- Week4RecursionBackTracking/ReverseString1.java
+++ Week4RecursionBackTracking/ReverseString1.java
@@ -1,5 +1,18 @@
 package Week4RecursionBackTracking;
 //Leet code 344. Reverse String
+/*
+ Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ */
+
+
 public class ReverseString1 {
     // naive 
     public static void reverseString(char[] s){
@@ -37,11 +50,25 @@ public class ReverseString1 {
             end--;
         }
     }
+
+    // approch 3 using recursion
+    public static void reverseString3(char[] s, int start, int end){
+        if (start >= end) {
+            return;
+        }
+        // swap
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+        reverseString3(s, start+1, end-1);
+    }
     public static void main(String[] args) {
         char s[] = {'h','e','l','l','o'};
         // printArr(s);
         // reverseString(s);
-        reverseString2(s);
+        // reverseString2(s);
+        reverseString3(s, 0, s.length-1);
         printArr(s);
 
     }
